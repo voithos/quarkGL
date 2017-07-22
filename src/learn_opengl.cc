d04be86c23d285d36621e6effb6297e86948362b
Zaven Muradyan <zaven@skepsi.me>
2017-07-22 16:32:52 -0700

    Add glm library to third party.

--- src/learn_opengl.cc
+++ src/learn_opengl.cc
@@ -3,6 +3,9 @@
 #include <cmath>
 #include <fstream>
 #include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <iostream>
 #include <sstream>
 #include <stb/stb_image.h>
