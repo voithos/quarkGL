9c835e3baad1d8bbb1808679e064651292ee80f7
Zaven Muradyan <zaven@skepsi.me>
2017-07-23 22:52:05 -0700

    Add camera movement.

--- src/learn_opengl.cc
+++ src/learn_opengl.cc
@@ -11,6 +11,13 @@
 #include <stb/stb_image.h>
 #include <string>
 
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
 void framebufferSizeCallback(GLFWwindow *window, int width, int height) {
   glViewport(0, 0, width, height);
 }
@@ -18,6 +25,21 @@ void framebufferSizeCallback(GLFWwindow *window, int width, int height) {
 void processInput(GLFWwindow *window) {
   if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
     glfwSetWindowShouldClose(window, true);
+  }
+  float cameraSpeed = 2.5f * deltaTime;
+  if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+    cameraPos += cameraSpeed * cameraFront;
+  }
+  if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+    cameraPos -=
+        cameraSpeed * glm::normalize(glm::cross(cameraFront, cameraUp));
+  }
+  if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+    cameraPos -= cameraSpeed * cameraFront;
+  }
+  if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+    cameraPos +=
+        cameraSpeed * glm::normalize(glm::cross(cameraFront, cameraUp));
   }
 }
 
@@ -178,15 +200,20 @@ int main() {
   unsigned int texture1 = createTexture("src/awesomeface.png");
 
   while (!glfwWindowShouldClose(window)) {
+    float currentFrame = glfwGetTime();
+    deltaTime = currentFrame - lastFrame;
+    lastFrame = currentFrame;
+
     processInput(window);
 
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glm::mat4 model =
-        glm::rotate(glm::mat4(), (float)glfwGetTime() * glm::radians(50.0f),
-                    glm::vec3(0.5f, 1.0f, 0.0f));
-    glm::mat4 view = glm::translate(glm::mat4(), glm::vec3(0.0f, 0.0f, -3.0f));
+    glm::mat4 model = glm::rotate(glm::mat4(), glm::radians(50.0f),
+                                  glm::vec3(0.5f, 1.0f, 0.0f));
+
+    glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+
     glm::mat4 projection = glm::perspective(
         glm::radians(45.0f), (float)screenWidth / (float)screenHeight, 0.1f,
         100.0f);
