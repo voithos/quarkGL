93b03743189fb0bc7cec78dd5eabd3717d22c707
Zaven Muradyan <zaven@skepsi.me>
2017-08-09 22:35:14 -0700

    Add VertexArray class.

---
+++ quarkgl/vertex_array.cc
@@ -0,0 +1,73 @@
+#include <qrk/vertex_array.h>
+
+namespace qrk {
+
+VertexArray::VertexArray() {
+  glGenVertexArrays(1, &vao_);
+  use();
+}
+
+void VertexArray::use() { glBindVertexArray(vao_); }
+
+void VertexArray::loadVertexData(const std::vector<char>& data) {
+  use();
+
+  glGenBuffers(1, &vbo_);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo_);
+  glBufferData(GL_ARRAY_BUFFER, data.size(), &data[0], GL_STATIC_DRAW);
+  vertexSize_ = data.size();
+}
+
+void VertexArray::loadVertexData(const char* data, unsigned int size) {
+  use();
+
+  glGenBuffers(1, &vbo_);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo_);
+  glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
+  vertexSize_ = size;
+}
+
+void VertexArray::loadElementData(const std::vector<unsigned int>& indices) {
+  use();
+
+  glGenBuffers(1, &ebo_);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo_);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size(), &indices[0],
+               GL_STATIC_DRAW);
+  elementSize_ = indices.size();
+}
+
+void VertexArray::loadElementData(const unsigned int* indices,
+                                  unsigned int size) {
+  use();
+
+  glGenBuffers(1, &ebo_);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo_);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, indices, GL_STATIC_DRAW);
+  elementSize_ = size;
+}
+
+void VertexArray::addVertexAttrib(unsigned int size, unsigned int type) {
+  VertexAttrib attrib = {(unsigned int)attribs_.size(), size, type};
+  attribs_.push_back(attrib);
+  // TODO: Support types other than float.
+  stride_ += size * sizeof(float);
+}
+
+void VertexArray::finalizeVertexAttribs() {
+  use();
+
+  int offset = 0;
+  for (const VertexAttrib& attrib : attribs_) {
+    glVertexAttribPointer(/* layout position */ attrib.layoutPosition,
+                          /* size */ attrib.size,
+                          /* type */ attrib.type,
+                          /* normalized */ GL_FALSE,
+                          /* stride */ stride_,
+                          // TODO: Support types other than float.
+                          /* offset */ static_cast<const char*>(0) + offset);
+    glEnableVertexAttribArray(attrib.layoutPosition);
+    offset += attrib.size * sizeof(float);
+  }
+}
+}
---
+++ quarkgl/vertex_array.h
@@ -0,0 +1,44 @@
+#ifndef VERTEX_ARRAY_H
+#define VERTEX_ARRAY_H
+
+#include <vector>
+
+#include <glad/glad.h>
+
+namespace qrk {
+
+class VertexArray {
+ private:
+  struct VertexAttrib {
+    unsigned int layoutPosition;
+    unsigned int size;
+    unsigned int type;
+  };
+
+  unsigned int vao_;
+  unsigned int vbo_;
+  unsigned int ebo_;
+
+  unsigned int vertexSize_ = 0;
+  unsigned int elementSize_ = 0;
+
+  std::vector<VertexAttrib> attribs_;
+  unsigned int stride_ = 0;
+
+ public:
+  VertexArray();
+  unsigned int getVao() { return vao_; }
+  unsigned int getVbo() { return vbo_; }
+  unsigned int getEbo() { return ebo_; }
+
+  void use();
+  void loadVertexData(const std::vector<char>& data);
+  void loadVertexData(const char* data, unsigned int size);
+  void loadElementData(const std::vector<unsigned int>& indices);
+  void loadElementData(const unsigned int* indices, unsigned int size);
+  void addVertexAttrib(unsigned int size, unsigned int type);
+  void finalizeVertexAttribs();
+};
+}
+
+#endif
